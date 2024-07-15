c38020bc8caf7ba82d7ce39dc890c725718f24cf
Grimaldi-Joao <joaogrimaldi6@gmail.com>
2024-07-15 11:40:31 -0300

    Feat: SpringBoot implementado

---
+++ src/test/java/com/Test/StafiBank/StafiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Test.StafiBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StafiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
